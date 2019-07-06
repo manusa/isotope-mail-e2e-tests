72b1a4fca64c122238b09c76336335e86b68ae25
Marc Nuri <marc@marcnuri.com>
2019-07-06 08:04:14 +0200

    manusa/isotope-mail#314: Parallel test exit code considers child processes

--- parallel-test.js
+++ parallel-test.js
@@ -8,12 +8,32 @@ const {deleteDirectory, mkdirRecursive} =  require('./utils');
 deleteDirectory(REPORTS_LOCATION);
 mkdirRecursive(REPORTS_LOCATION);
 
+let globalExitCode = 0;
+const finishedProcesses = [];
+
 for (let browser in BROWSERS) {
   const env = {...process.env};
   env.BROWSER_ID = browser;
   console.log(`Launching child process for ${browser} tests`);
-  childProcess.fork('./node_modules/cucumber/bin/cucumber-js', [], {
+  const child = childProcess.fork('./node_modules/cucumber/bin/cucumber-js', [], {
     env,
     stdio: 'inherit'
   });
+  child.on('exit', (code) => {
+    console.log(`Child process for ${browser} finished with code ${code}`);
+    if (code > 0){
+      globalExitCode = 1;
+    }
+    finishedProcesses.push(browser);
+  });
 }
+
+async function waitForCompletion() {
+  while (finishedProcesses.length !== Object.keys(BROWSERS).length) {
+    await  new Promise(done => setTimeout(done, 500));
+  }
+  console.log(`All browser processes have completed!`)
+  process.exitCode = globalExitCode;
+}
+
+waitForCompletion();
